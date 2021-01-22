4f9cb9b052b424bffd0e49552a34a01f202c9937
Erik Gomersbach <erik@gomersba.ch>
2021-01-22 23:54:16 +0100

    use project name as name of target pdf

--- The Legrand Orange Book.tex/compile.sh
+++ The Legrand Orange Book.tex/compile.sh
@@ -3,18 +3,26 @@
 # exit when any command fails
 set -e
 
-while getopts t: flag
-do
-    case "${flag}" in
-        t) target=${OPTARG};;
-    esac
+while getopts ":n:t:" opt; do
+  case ${opt} in
+    n  ) name=${OPTARG};;
+    t  ) target=${OPTARG};;
+    \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+    :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+    *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+  esac
 done
-echo "${target}"
+
+if ((OPTIND == 1))
+then
+  echo "No options specified"
+  exit 1
+fi
 
 SCRIPT_DIR="$(dirname "$(stat -f "$0")")"
 cd "${SCRIPT_DIR}"
-pdflatex -halt-on-error ${target}
-makeindex ${target}.idx -s StyleInd.ist
-biber ${target}
-pdflatex -halt-on-error ${target} x 2
-open ${target}.pdf
+pdflatex -halt-on-error -jobname="${name}" "${target}"
+makeindex "${name}.idx" -s StyleInd.ist
+biber "${name}"
+pdflatex -halt-on-error -jobname="${name}" "${target}" x 2
+open "${name}.pdf"
